77d4202e10c6ee2b95b5d2f15ae32d2cae6a6ccd
Dawit Mellese <dawitm777@gmail.com>
2024-11-14 21:43:36 +0300

    bug fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
@@ -12,34 +12,36 @@ import ConnectWallet from "./components/ConnectWallet";
 import { MainProvider } from "./context/MainContext";
 import CategoryDetailPage from "./pages/CategoryDetailPage";
 
+function AppContent() {
+  const location = useLocation();
+
+  return (
+    <div className="bg-gray-50">
+      <ScrollToTop />
+      {location.pathname !== "/connect-wallet" && <Header />}
+      
+      <Routes>
+        <Route path="/connect-wallet" element={<ConnectWallet />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/product/category/:id" element={<ProductDetail />} />
+          <Route path="/product/category" element={<CategoryDetailPage />} />
+        </Route>
+      </Routes>
+
+      {location.pathname !== "/connect-wallet" && <Footer />}
+    </div>
+  );
+}
+
 function App() {
   return (
     <MainProvider>
       <BrowserRouter>
-        <div className="bg-gray-50">
-          <Header />
-          <ScrollToTop />
-          <div className="min-h-screen">
-            <Routes>
-              <Route path="/connect-wallet" element={<ConnectWallet />} />
-              <Route element={<PrivateRoute />}>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/about" element={<AboutPage />} />
-                <Route path="/products" element={<ProductsPage />} />
-                <Route path="/contact" element={<ContactPage />} />
-                <Route
-                  path="/product/category/:id"
-                  element={<ProductDetail />}
-                />
-                <Route
-                  path="/product/category"
-                  element={<CategoryDetailPage />}
-                />
-              </Route>
-            </Routes>
-          </div>
-          <Footer />
-        </div>
+        <AppContent />
       </BrowserRouter>
     </MainProvider>
   );
